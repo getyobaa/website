b8592e0c03b6ae6a6e48a71b2c1272230f6ed068
Samson <horppes.web@gmail.com>
2024-11-13 10:15:33 +0100

    updated footer

--- src/components/Layout/Website/WebsiteFooter/WebsiteFooter.tsx
+++ src/components/Layout/Website/WebsiteFooter/WebsiteFooter.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './index.module.css';
+import './index.css';
 export default function WebsiteFooter() {
     return (
         <footer className={styles.footer}>
